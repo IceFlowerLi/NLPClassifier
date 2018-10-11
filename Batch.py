92309ea1803c41fa61737bc28f517f02935a5a1d
Li <1275626721@qq.com>
2018-10-11 21:31:20 +0800

    Solve the label iter bug and keep the small length of the sentence.

--- Batch.py
+++ Batch.py
@@ -26,7 +26,10 @@ class Iterator(BatchFeature):
         super(BatchFeature, self).__init__()
 
         self.batch = batch
+
+        assert batch_len < len(data), "ERROR! Batch size over the data length!"
         self.batch_len = batch_len
+
         self.data = [data] if type(data) != list else data
         self.vocab = vocab
         self.config = config
@@ -48,14 +51,12 @@ class Iterator(BatchFeature):
             per_stc = []
 
             for idx, word in enumerate(sentence):
-                if idx < self.batch.sentence_maxlen:
-                    per_stc.append(vocab.load_word2id(word.lower()))
-                else:
-                    per_stc.append(vocab.pad_idx)
+                per_stc.append(vocab.load_word2id(word.lower()))
 
-            word_gap = self.batch.sentence_maxlen - len(sentence)
-            if word_gap > 0:
-                per_stc = per_stc + [vocab.pad_idx for i in range(word_gap)]
+            # Keep the same length of sentence.
+            # word_gap = self.batch.sentence_maxlen - len(sentence)
+            # if word_gap > 0:
+            #     per_stc = per_stc + [vocab.pad_idx for i in range(word_gap)]
 
             wordid.append(per_stc)
             labelid.append(vocab.load_label2id(label))
@@ -68,7 +69,7 @@ class Iterator(BatchFeature):
 
         word_vct, label_vct = self._word_id(inst, vocab)
 
-        for group in range(len(word_vct) // self.batch_len):
+        for group in range(len(word_vct) // self.batch_len + 1):
 
             start, end = group * self.batch_len, (group + 1) * self.batch_len
 
@@ -82,9 +83,9 @@ class Iterator(BatchFeature):
 
                 for i in range(extra):
                     supply_word.append(
-                        [vocab.pad_idx for i in inst.sentence_maxlen]
+                        [vocab.pad_idx for i in range(self.batch.sentence_maxlen)]
                     )
-                    supply_label.append(vocab.unk_idx)
+                    supply_label.append(vocab.load_label2id('Unkown'))
 
                 perbatch_word.append(word_vct[start:] + supply_word)
                 perbatch_label.append(label_vct[start:] + supply_label)
@@ -114,7 +115,7 @@ class Iterator(BatchFeature):
         for sgl_data in self.data:
             dt_it, lb_it = self.create_perbatch(sgl_data, self.vocab)
             data_iter.append(dt_it)
-            label_iter.append(label_iter)
+            label_iter.append(lb_it)
 
         return data_iter, label_iter
 
@@ -126,9 +127,10 @@ if __name__ == '__main__':
     table = WordTable(word.vocab, word.sentence_maxlen)
     table.build_table()
 
-    batch_iter = Iterator(batch=word, batch_len=50,
+    batch_iter = Iterator(batch=word, batch_len=500,
                           data=word.sentence_label, vocab=table)
-    data_iter = batch_iter.create_iter()
+    data_iter, label_iter = batch_iter.create_iter()
+    print('')
 
 
 
